8b16bb765ebfecac65f266e9dd0bc2dd21e62084
Oriol López Sánchez <mithrandir0x@gmail.com>
2016-10-12 20:01:12 +0200

    Modified 'kafka' init.d service file to be used from the HDP release.

--- modules/kafka_server/manifests/init.pp
+++ modules/kafka_server/manifests/init.pp
@@ -23,11 +23,6 @@ class kafka_server {
   package { "kafka" :
     ensure => installed,
   }
-  service { 'kafka':
-    ensure => running,
-    enable => true,
-    require => Package['kafka'],
-  }
 
   # Configure.
   file { '/etc/kafka/conf/consumer.properties':
@@ -36,19 +31,21 @@ class kafka_server {
     before => Service['kafka'],
     require => Package['kafka'],
   }
+  ->
   file { '/etc/kafka/conf/producer.properties':
     ensure => file,
     content => template('kafka_server/producer.properties.erb'),
     before => Service['kafka'],
     require => Package['kafka'],
   }
+  ->
   file { '/etc/kafka/conf/server.properties':
     ensure => file,
     content => template('kafka_server/server.properties.erb'),
     before => Service['kafka'],
     require => Package['kafka'],
   }
-
+  ->
   # Create a topic called test.
   file { "/tmp/create_test_topic.sh":
     ensure => "file",
@@ -71,12 +68,22 @@ class kafka_server {
       source => "/vagrant/files/systemd/kafka.service.d/default.conf",
       before => Service["kafka"],
     }
+    ->
+    service { 'kafka':
+      ensure => running,
+      enable => true,
+      require => Package['kafka'],
+    }
   } else {
-    file { "/etc/init.d/kafka":
-      ensure => file,
-      source => 'puppet:///modules/kafka_server/kafka',
-      replace => true,
-      before => Service['kafka'],
+    exec { 'kafka-create-service':
+      command => 'cp /usr/hdp/current/kafka-broker/bin/kafka /etc/init.d/',
+      path => $path,
+    }
+    ->
+    service { 'kafka':
+      ensure => running,
+      enable => true,
+      require => Package['kafka'],
     }
   }
 }
